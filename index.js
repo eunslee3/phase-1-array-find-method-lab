2c5720d6935e697066bb094dabf991eb728c5eec
Samuel <lee.eunseong7856@gmail.com>
2022-11-01 18:01:33 -0500

    commit tree fa5cb84ae3c36869dc256cce82cf467a25420288

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(array) {
+    for(const newRecord of array) {
+        if(newRecord.result === "W")
+            return newRecord.year;
+    }
+}
+superbowlWin(record);
+
+//use find() to test each object and see if the result is "w" 
+// maybe use for...of? or a whole bunch of if statements
+// lets try using the for...of loop first
